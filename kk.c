a5a076ff1ab66299fed2479667c12a8799e86bf8
dongfangshuo <gotoworld00@gmail.com>
2013-11-28 14:38:25 +0800

    kk

--- kk.c
+++ kk.c
@@ -3,7 +3,7 @@ void main()
 {
 	printf("test");
 	int kk[]= {34,34,34,23,3,34,34,56,767,675,45},*ll=kk;
-	int *index=&kk[4];
+	int *index=&kk[4],*k2= &kk[2],*k5 = &kk[5],*k8 = &kk[8],*k10 = &kk[10];
 	printf("kk[%d] = %d",3,*(ll+3));
 	putchar('\n');
 	printf("kk[%d]= %d",4,*index);
@@ -14,4 +14,12 @@ void main()
 	putchar('\n');
 	ll = ll+4;
 	printf("ll+1=%d\n", *ll );
+	printf("============================================================\n");
+	if(k8 > k10)
+	{
+	printf("k8 > k10");
+	}else{
+ 	printf("k10 > k8");	
+	}
+	putchar('\n');
 }
