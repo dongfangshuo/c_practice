0a479dc3efbc3f545d264a5fdeeb117080b1b82d
dongfangshuo <gotoworld00@gmail.com>
2013-12-10 23:24:10 +0800

    d

---
+++ data_structure/ListInserter.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "data.c"
+void  InsertList(SeqList *l,int i ,DataType x)
+{
+	int j,temp,*data,mid;
+	if(i < 1 || i > l->length + 1)	
+	{
+		printf("index %i is not valid \n",i);	
+		return;
+	}
+	if(l->length >= ListSize)
+	{
+		printf("overflow\n");		
+		return; 
+	}
+	data = l->data;
+	for(j = 0; j <= l->length;j++ )	
+	{
+		if(j >= i-1 )	
+		{
+			mid = data[j];
+			data[j] = temp;
+			temp = mid;
+		}
+	}
+	data[i-1] = x;
+	l->length++;
+}
+void showData(SeqList *l)
+{
+	int i,len,*data; 
+	data = l->data;	
+	printf("seqlist length is %d \n ",l->length);
+	for(i = 0,len = l->length; i < len;i++)
+	{
+		printf("the index %d => %d \n",i,*(data+i));	
+	}
+}
+void main()
+{
+	SeqList s,*l;
+	DataType data = 8;
+	l = &s;
+//	s.length = 10; 
+	printf("=================%d,%d \n",s.length,ListSize);
+	InsertList(l,1,data);
+	InsertList(l,2,18);
+	InsertList(l,3,156);
+	showData(l);
+	InsertList(l,2,8888);
+	showData(l);
+}
---
+++ data_structure/data.c
@@ -0,0 +1,6 @@
+#define ListSize 100
+typedef int DataType;
+typedef struct{
+	DataType data[ListSize];	
+	int length;
+}SeqList;
