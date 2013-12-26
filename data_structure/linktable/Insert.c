2d9e03845bc3d2f1e62cdde9f39ec43f337b84b1
dongfangshuo <gotoworld00@gmail.com>
2013-12-26 18:47:37 +0800

    Insert.c

--- data_structure/linktable/Insert.c
+++ data_structure/linktable/Insert.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "node.c"
-LinkList headInsert(DataType *dtp,int size)
+//有头节点
+LinkList headInsert1(DataType *dtp,int size)
 {
 	LinkList head;		
 	int i; 
@@ -20,9 +21,41 @@ LinkList headInsert(DataType *dtp,int size)
 	}
 	return head;
 }
+//不用头节点
+LinkList headInsert2(DataType *dtp,int size)
+{
+	LinkList head = NULL;		
+	int i; 
+	DataType val;
+	ListNode *node;
+	for(i = 0; i < size; i++)	
+	{
+		node  = (ListNode *)malloc(sizeof(ListNode));	
+		val =*(dtp+i); 
+		if(head ==NULL)
+		{
+			head = node;		
+			head->data = val;
+			head->next=NULL;
+		}else{
+			node->data = head->data;
+			head->data = val;
+			node->next = head->next; 
+			head->next = node;
+		}
+	}
+	return head;
+}
+LinkList tailInsert(DataType *dtp,int size)
+{
+	LinkList tail;	
+	int i;
+	DataType val;
+
+}
 void showLink(LinkList head)
 {
-	head = head->next;	
+//	head = head->next;	
 	while(head->next != NULL)
 	{
 		printf("%d\n",head->data);	
@@ -36,6 +69,6 @@ void main()
 	LinkList head;
 	DataType ds[10] = {0,1,2,3,4,5,6,7,8,9}, *dtp  = ds;
 	printf("main \n");
-	head = headInsert(dtp,size);
+	head = headInsert2(dtp,size);
 	showLink(head);
 }
